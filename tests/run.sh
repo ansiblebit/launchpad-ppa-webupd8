602e5adc41a1b0ab3d1ed9fe3f0c06e92111e2bc
steenzout <steenzout@ymail.com>
2015-04-04 11:22:45 -0600

    added test resources.

--- tests/run.sh
+++ tests/run.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 
+PYTHON_VERSION=27
+ANSIBLE_VERSION=182
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+DIR="$(dirname "$0")"
 
-# syntax test
-OUTPUT=`ansible-playbook --syntax-check -i inventory/local test.yml 2>&1`
-RETURN_CODE=$?
-if [[ $RETURN_CODE != 0 ]]; then
-  echo -ne "Syntax test: ${RED}FAIL${NC}\n"
-  echo $OUTPUT
-  exit $RETURN_CODE
-else
-  echo -ne "Syntax test: ${GREEN}PASS${NC}\n"
-fi
+cd $DIR
 
+source ../.tox/py${PYTHON_VERSION}-ansible${ANSIBLE_VERSION}/bin/activate
 
-# playbook run test
+for box in precise64 trusty64 wheezy64
+do
+    vagrant up $box
+    
+    ansible-playbook -i inventory/vagrant -l $box.vagrant.dev test.yml
+    RETURN_CODE=$?
+    if [[ $RETURN_CODE != 0 ]]; then
+      echo -ne "Playbook run test: ${RED}FAILED${NC}\n"
+      exit $RETURN_CODE
+    else
+      echo -ne "Playbook run test: ${GREEN}PASS${NC}\n"
+    fi
 
-ansible-playbook -i inventory/local test.yml
-RETURN_CODE=$?
-if [[ $RETURN_CODE != 0 ]]; then
-  echo -ne "Playbook run test: ${RED}PASS${NC}\n"
-  exit $RETURN_CODE
-else
-  echo -ne "Playbook run test: ${GREEN}PASS${NC}\n"
-fi
+    ansible-playbook -i inventory/vagrant -l $box.vagrant.dev test.yml | \
+        grep -q 'changed=0.*failed=0' && \
+        echo -ne "[$box] idempotence test: ${GREEN}PASS${NC}\n" || \
+        (echo -ne "[$box] idempotence test: ${RED}FAILED${NC}\n" && exit 1)
 
-
-# playbook idempotency test
-
-ansible-playbook -i inventory/local test.yml | \
-    grep -q 'changed=0.*failed=0' && \
-    (echo -ne "Idempotence test: ${GREEN}PASS${NC}\n" && exit 0) || \
-    (echo -ne "Idempotence test: ${RED}FAIL${NC}\n" && exit 1)
-
+    vagrant destroy -f $box
+done
